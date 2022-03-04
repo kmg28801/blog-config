763cce8f2651bebfe4f182b12a361771ae665441
kmg28801 <kmg08801@gmail.com>
2022-03-04 20:18:32 +0900

    config service initialize

---
+++ src/main/java/tj/kmg/blog/config/BlogConfigApplication.java
@@ -0,0 +1,16 @@
+package tj.kmg.blog.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@EnableConfigServer
+@SpringBootApplication
+public class BlogConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tj/kmg/blog/config/BlogConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package tj.kmg.blog.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
